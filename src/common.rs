f50b5dc7fe64428bd3a0874369ce6e65a00829fc
zbsun2008 <sunzhaofeng@qq.com>
2026-02-07 15:28:02 +0800

    更新配置文件

--- src/common.rs
+++ src/common.rs
@@ -1081,7 +1081,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "https://rust.tpdns.cn".to_owned()
 }
 
 #[inline]
